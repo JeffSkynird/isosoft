9ff2211348012d3bc003a213af47718c7cf3aa06
jefferson.leon <jefferson.leon@apanbla.com>
2021-09-04 00:58:50 -0500

    cambio logo

--- client/src/containers/main/Bienvenida.js
+++ client/src/containers/main/Bienvenida.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import logo from '../../assets/logoPeque.png'
-import splash from '../../assets/bienvenida.png'
+import splash from '../../assets/bienvenida2.png'
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { useTheme } from '@material-ui/core/styles';
 
@@ -12,31 +12,32 @@ export default function Bienvenida(props) {
 
     const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
     return (
-        <div style={{height:'100%',width:'100%'}}>
-            <div style={{ display: 'flex', flexDirection:fullScreen?'column':'row',alignItems:fullScreen?'center':'',justifyContent: 'space-between',padding:15 }}>
+        <div style={{ height: '100%', width: '100%' }}>
+            <div style={{ display: 'flex', flexDirection: fullScreen ? 'column' : 'row', alignItems: fullScreen ? 'center' : '', justifyContent: 'space-between', padding: 15 }}>
                 <img src={logo} style={{ marginBottom: 15, height: 30, width: 124 }} alt="" srcset="" />
 
                 <div>
-                    <Button style={{marginRight:10}}  onClick={() => props.history.push('/login',{register:true})}>Registrarse</Button>
+                    <Button style={{ marginRight: 10 }} onClick={() => props.history.push('/login', { register: true })}>Registrarse</Button>
                     <Button variant="contained" color="primary" onClick={() => props.history.push('/login')}>
                         Iniciar Sesión
                     </Button>
                 </div>
             </div>
-            <div style={{display:'flex',justifyContent:'space-between',alignItems:'center'}}>
-                <div style={{width:fullScreen?'100%':'40%',paddingLeft:20}}>
-                <Typography variant="h3" >
-            Califica con check lists de <span style={{color:'#186FFD'}}>cumplimiento</span>  
-                </Typography>     
-                <Typography variant="h5" style={{fontWeight:100,marginTop:10,color:'#929396',marginBottom:15}} >
-                La norma ISO/IEC 25010 es una norma internacional que se enfoca en la evaluación de calidad de software y sistemas. Conocida como Square (Requisitos y evaluación de la calidad de los sistemas y el software). 
+            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <div style={{ width: fullScreen ? '100%' : '40%', paddingLeft: 30 }}>
+                    <Typography variant="h5" >
+                        Modelo de calidad de Software
+                    </Typography>
+                    <Typography variant="h3" style={{ color: '#186FFD', fontWeight: 'bold' }}>
+                        ISO 25010
+                    </Typography>
+                    <Typography variant="subtitle1" style={{ fontWeight: 100, marginTop: 10, color: '#929396', marginBottom: 15 }} >
+                        La norma ISO/IEC 25010 es una norma internacional que se enfoca en la evaluación de calidad de software y sistemas,  es un conjunto de estándares de calidad para evaluar productos de software por medio de la evaluación de varias características: adecuación funcional, compatibilidad, usabilidad, fiabilidad, seguridad, mantenibilidad, portabilidad y eficiencia en el desempeño.
 
-                </Typography>
-                <Button variant="outlined" color="primary" size="large" onClick={() => props.history.push('/login')}>
-                        Empezar
-                    </Button>
+                    </Typography>
+
                 </div>
-                <img src={splash} style={{width:700,height:625}} alt="" srcset="" />
+                <img src={splash} style={{ width: 700, height: 625 }} alt="" srcset="" />
 
 
             </div>
